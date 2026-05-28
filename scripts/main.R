6487578ef65e7957372c99731cd90d2dccaaea06
Uday Rangaswamy <IEO7976@Udays-MacBook-Air.local>
2026-05-28 18:26:01 +0200

    Initial DESeq2 analysis project

---
+++ scripts/main.R
@@ -0,0 +1,14 @@
+packages <- c(
+  "tidyverse",
+  "ggplot2"
+)
+
+installed <- packages %in% installed.packages()
+
+if(any(!installed)) {
+  install.packages(packages[!installed])
+}
+
+lapply(packages, library, character.only = TRUE)
+
+source("sig_DEGs.R")
